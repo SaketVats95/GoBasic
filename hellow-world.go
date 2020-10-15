8e467e35db7a8b474c24a783406e6481626b4981
Saket Vats <44232561+SaketVats95@users.noreply.github.com>
2020-10-15 22:51:14 +0530

    New Method to return sum of two numbers

--- hellow-world.go
+++ hellow-world.go
@@ -10,4 +10,13 @@ func main() {
 		fmt.Println("Saket ")
 
 	}
+	n1 := 10
+	n2 := 20
+	result := add(n1, n2)
+	fmt.Println("Sum of Two numbers are :", result)
+}
+
+func add(num1 int, num2 int) int {
+	var result = num1 + num2
+	return result
 }
